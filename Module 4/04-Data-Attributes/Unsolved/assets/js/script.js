153ba61c6018a446f512b61094ac23fe25959a56
acamp <acampbell8610@gmail.com>
2022-01-13 20:24:43 -0600

    class

--- Module 4/04-Data-Attributes/Unsolved/assets/js/script.js
+++ Module 4/04-Data-Attributes/Unsolved/assets/js/script.js
@@ -4,4 +4,16 @@ container.addEventListener("click", function(event) {
   var element = event.target;
 
   // TODO: Complete function
+
+console.log(element);
+
+
+if(element.textContent){
+ // element.textContent = '';
+  element.setAttribute('data-state', 'hidden');
+}else{
+  element.textContent= element.dataset.number;
+  //element.dataset.state = "visible";
+  element.getAttribute('data-state', 'visible')
+}
 });
