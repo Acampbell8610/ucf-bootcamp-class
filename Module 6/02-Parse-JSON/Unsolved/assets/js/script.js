f2983a4aefe0217678e523406ff6eff3a1bdf8d0
acamp <acampbell8610@gmail.com>
2022-01-25 19:50:15 -0600

    mod 6 class 1

---
+++ Module 6/02-Parse-JSON/Unsolved/assets/js/script.js
@@ -0,0 +1,18 @@
+// TODO: Edit the URL to get only 5 issues of Twitter's Chill repo
+var requestUrl = 'https://api.github.com/repos/Twitter/CHILL/issues?per_page=5';
+
+fetch(requestUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log('Github Repo Issues \n----------');
+    
+    // TODO: Loop through the response
+    // TODO: Console log each issue's URL and each user's login
+    for(var i = 0; i < data.length ; i++){
+    
+    console.log(data[i].url);
+    console.log(data[i].user.login)
+  }
+  });
